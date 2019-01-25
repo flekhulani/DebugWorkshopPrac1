0261cac657383430fb3e2dd9884e22164d95da21
flekhulani <flekhulani@retrorabbit.co.za>
2019-01-25 10:33:13 +0200

    Swopped the two statements that use temp in partition

--- Program.cs
+++ Program.cs
@@ -51,8 +51,8 @@ namespace debugws
           }
 
           int temp = arr[left];
-          arr[right] = temp;
           arr[left] = arr[right];
+           arr[right] = temp;
         }
         else
         {
